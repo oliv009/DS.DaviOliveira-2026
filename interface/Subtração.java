c4989f9f24ac778d03a249a1ad6c021904acd3d5
Davi Oliveira <davioliveiradasilva09@gmail.com>
2026-06-11 14:25:34 -0300

    11/06

---
+++ interface/Subtração.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkginterface;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Subtração implements OperaçãoMatematica {
+
+    @Override
+    public int calcula(int a, int b) {
+        return a-b;
+    }
+    
+}
